23ba443c971e8480eda49967095f8e18bb611c8c
Kristijan Jovanovski <kristijan.jovanovski@pcg.io>
2026-01-31 15:36:55 +0100

    update deployment workflows to conditionally invalidate CloudFront and replace vite.svg with favicon.svg; update header to include navigation link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router";
+
 function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-[#0e0f1c]/95 backdrop-blur-sm border-b border-gray-800">
       <nav className="container mx-auto px-6 py-6 flex items-center justify-between">
-        <div className="md:text-2xl text-xl font-bold">
-          <span className="text-amber-400 font-semibold">AWS</span> Cloud{" "}
-          <span className="text-teal-400 font-semibold">Engineer</span>
-        </div>
+        <Link to="/">
+          <div className="md:text-2xl text-xl font-bold">
+            <span className="text-amber-400 font-semibold">AWS</span> Cloud{" "}
+            <span className="text-teal-400 font-semibold">Engineer</span>
+          </div>
+        </Link>
         <ul className="flex gap-8">
           <li>
             <a href="#hero" className="hover:text-teal-400 transition">
